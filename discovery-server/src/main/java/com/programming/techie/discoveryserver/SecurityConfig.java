3eac403351462decf59f69463c8be3ecade025ce
chandu oggu <yaswanthinaganti9@gmail.com>
2024-09-13 15:36:36 +0530

    first commit

---
+++ discovery-server/src/main/java/com/programming/techie/discoveryserver/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.programming.techie.discoveryserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.ignoringRequestMatchers("/eureka/**"));
+        return http.build();
+    }
+}
